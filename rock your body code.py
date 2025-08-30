453a05d912b866ce179acba3f40966385735fd49
Devang <devdaksh3101@gmail.com>
2025-08-30 18:19:49 +0530

    new file:   rock your body code.py

---
+++ rock your body code.py
@@ -0,0 +1,26 @@
+#Don't For get to run pip install rich on terminal. It will intall all important file to run it.
+
+import sys
+from rich import print
+from time import sleep
+import time
+
+def printLyrics():
+    lines = [
+        ('I wanna da-', 0.06), #1
+        ('I wanna dance in the lights', 0.05), #2
+        ('I wanna ro-', 0.07), #3
+        ('I wanna rock your body', 0.08), #4
+        ('I wanna go-', 0.08), #5
+        ('I wanna go for a ride', 0.68), #6
+        ('Hop in the music and', 0.07), #7
+        ('Rock your body', 0.08), #8
+        ('Rock that body', 0.069), #9
+        ('Come on, come on', 0.035), #10
+        ('Come on, come on', 0.05), #11
+        ('(Rock your body)', 0.03), #12
+        ('Rock that body', 0.049), #13
+        ('Come on, come on)', 0.035), #14
+        ('Rock that body', 0.08), #15
+
+    ]
